097961fabc9422116a81b51eef2a9916c4f4bcb7
Uku Taht <uku.taht@gmail.com>
2023-10-20 19:08:08 +0300

    Add transitions & phx-click-away

--- assets/js/hooks/dropdown.js
+++ assets/js/hooks/dropdown.js
@@ -4,16 +4,12 @@ export default {
     const menu = this.el.querySelector('[role="menu"]')
     const items = this.el.querySelectorAll('[role="menuitem"]')
 
-    this.refs = {
-      button,
-      menu,
-      items
-    }
+    this.refs = { button, menu, items }
 
     button.addEventListener('click', this.toggle.bind(this))
   },
 
   toggle(e) {
-    liveSocket.execJS(this.refs.menu, this.refs.menu.getAttribute("js-open"))
+    liveSocket.execJS(this.refs.menu, this.refs.menu.getAttribute("js-toggle"))
   }
 }
--- lib/livekit/dropdown.ex
+++ lib/livekit/dropdown.ex
@@ -5,39 +5,52 @@ defmodule Livekit.Dropdown do
   slot :id, required: true
   slot :inner_block, required: true
 
+  # TODO: phx-click-away ID is hardcoded
   def dropdown(assigns) do
     ~H"""
-    <div id={@id} phx-hook="Dropdown">
+    <div id={@id} phx-hook="Dropdown" phx-click-away={JS.exec("js-hide", to: "#dropdown-items")}>
       <%= render_slot(@inner_block) %>
     </div>
     """
   end
 
+  attr :class, :string, default: ""
   slot :inner_block, required: true
 
   def dropdown_button(assigns) do
     ~H"""
-    <button type="button" aria-haspopup="menu">
+    <button class={@class} type="button" aria-haspopup="menu">
       <%= render_slot(@inner_block) %>
     </button>
     """
   end
 
+  attr :transition_enter, :any, default: {}
+  attr :transition_leave, :any, default: {}
+  attr :class, :string, default: ""
   slot :inner_block, required: true
 
   def dropdown_items(assigns) do
     ~H"""
-    <div style="display: none" js-open={JS.toggle()} role="menu">
+    <div
+      class={@class}
+      style="display: none"
+      js-toggle={JS.toggle(in: @transition_enter, out: @transition_leave)}
+      js-hide={JS.hide(transition: @transition_leave)}
+      role="menu"
+      id="dropdown-items"
+    >
       <%= render_slot(@inner_block) %>
     </div>
     """
   end
 
+  attr :class, :string, default: ""
   slot :inner_block, required: true
 
   def dropdown_item(assigns) do
     ~H"""
-    <div role="menuitem">
+    <div class={@class} role="menuitem">
       <%= render_slot(@inner_block) %>
     </div>
     """
