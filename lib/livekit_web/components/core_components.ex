f45835167c695ded380791a04d5fffa5524e7806
Uku Taht <uku.taht@gmail.com>
2025-06-20 16:46:21 +0300

    Run upgrade tool

--- lib/livekit_web/components/core_components.ex
+++ lib/livekit_web/components/core_components.ex
@@ -255,7 +255,7 @@ defmodule LivekitWeb.CoreComponents do
         name={@name}
         class={[
           "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          "min-h-[6rem] phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+          "min-h-24 phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
