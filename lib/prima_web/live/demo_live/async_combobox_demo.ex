f6e356cbebd291651f8580605b64ded405fae6c2
Uku Taht <uku.taht@gmail.com>
2025-10-03 16:34:03 +0300

    More consistent styling for combobox demos

--- lib/prima_web/live/demo_live/async_combobox_demo.ex
+++ lib/prima_web/live/demo_live/async_combobox_demo.ex
@@ -29,7 +29,7 @@ defmodule PrimaWeb.DemoLive.AsyncComboboxDemo do
         <div class="relative mt-2 rounded-md shadow-sm">
           <.combobox_input
             name="user[favourite_fruit]"
-            class="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 cursor-default placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 peer"
+            class="block w-full rounded-md border-0 py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 peer"
             phx-change="async_combobox_search"
             phx-target={@myself}
             placeholder="Type to search..."
