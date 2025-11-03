5bb366f2304b38f73d77e3bd9e228fc748dcdb22
Uku Taht <uku.taht@gmail.com>
2025-11-03 17:11:46 +0200

    Adjust package contents

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Prima.MixProject do
       name: "Prima",
       description: "Unstyled, accessible components for LiveView applications",
       app: :prima,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -53,7 +53,7 @@ defmodule Prima.MixProject do
     [
       licenses: ["MIT"],
       links: %{"GitHub" => @source_url},
-      files: ~w(lib priv assets/js mix.exs README.md LICENSE)
+      files: ~w(lib priv mix.exs README.md LICENSE package.json)
     ]
   end
 
