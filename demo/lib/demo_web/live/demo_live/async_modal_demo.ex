08b31ec13251adf4d310c5059b18211d3d32df80
Uku Taht <uku.taht@gmail.com>
2025-11-05 21:50:30 +0200

    Organize open/close commands better

--- demo/lib/demo_web/live/demo_live/async_modal_demo.ex
+++ demo/lib/demo_web/live/demo_live/async_modal_demo.ex
@@ -17,7 +17,7 @@ defmodule DemoWeb.DemoLive.AsyncModalDemo do
         id="open-form-modal-button"
         type="button"
         phx-click={
-          Prima.Modal.open("demo-form-modal") |> JS.push("open-async-modal", target: @myself)
+          Prima.Modal.JS.open("demo-form-modal") |> JS.push("open-async-modal", target: @myself)
         }
         class="inline-flex justify-center rounded-md bg-indigo-600 disabled:bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
@@ -71,7 +71,7 @@ defmodule DemoWeb.DemoLive.AsyncModalDemo do
               <div class="relative">
                 <button
                   class="absolute top-0 right-0"
-                  phx-click={Prima.Modal.close()}
+                  phx-click={Prima.Modal.JS.close()}
                   testing-ref="close-button"
                 >
                   <svg
