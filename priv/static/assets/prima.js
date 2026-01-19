c61919724cd05b131bfa763ba04fb8a37ff1fb23
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-01-19 10:43:34 +0100

    Update assets

--- priv/static/assets/prima.js
+++ priv/static/assets/prima.js
@@ -1646,6 +1646,7 @@ var modal_default = {
     ];
   },
   setupElements() {
+    this.autoClose = Object.hasOwn(this.el.dataset, "primaAutoClose");
     if (!this.ref("modal-panel")) {
       this.async = true;
     }
@@ -1720,17 +1721,22 @@ var modal_default = {
       this.el.dispatchEvent(new Event("prima:modal:close"));
     }
   },
-  handleModalClose() {
-    this.restoreBodyScroll();
-    this.maybeExecJS(this.ref("modal-overlay"), "js-hide");
-    this.maybeExecJS(this.ref("modal-panel"), "js-hide");
-    this.maybeExecJS(this.ref("modal-loader"), "js-hide");
-    if (this.async) {
-      this.ref("modal-panel").dataset.primaDirty = true;
+  handleModalClose(e) {
+    if (this.autoClose || !e) {
+      this.restoreBodyScroll();
+      this.maybeExecJS(this.ref("modal-overlay"), "js-hide");
+      this.maybeExecJS(this.ref("modal-panel"), "js-hide");
+      this.maybeExecJS(this.ref("modal-loader"), "js-hide");
+      if (this.async) {
+        this.ref("modal-panel").dataset.primaDirty = true;
+      }
+    } else {
+      this.maybeExecJS(this.el, "js-hide");
     }
   },
   handleOverlayHideEnd() {
     this.maybeExecJS(this.el, "js-hide");
+    this.js().hide(this.el);
     this.el.setAttribute("aria-hidden", "true");
     this.restoreFocusedElement();
   },
